60127beef86e3faa3cce52a82b90d5942f4d77e4
luuthienbao <baoluuthien1996@gmail.com>
2021-03-21 16:36:03 +0700

    first init project

---
+++ presentation/src/main/java/com/thienbao/kotlincleanarchitechture/NewsActivity.kt
@@ -0,0 +1,11 @@
+package com.thienbao.kotlincleanarchitechture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
